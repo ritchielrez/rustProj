9b1a81b528c3f76f410f01b73f0e0ea722eac04b
RitchielRez <ritchielrez@gmail.com>
2021-09-16 19:22:54 +0600

    Updated

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
     let mut x = 185;
 
+    // Named arguments
     println!("{num}", num = x);
+
+    let spaces = "  ";
+    println!("Spaces: {}", spaces);
+
+    let spaces = spaces.len();
+    println!("Spaces: {}", spaces);
 }
