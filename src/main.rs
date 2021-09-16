4d4ec6f0e34909b8e84114df19d9482b18abb5db
RitchielRez <ritchielrez@gmail.com>
2021-09-16 21:45:09 +0600

    Tuples and Arrays

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,12 @@ fn main() {
 
     let spaces = spaces.len();
     println!("Spaces: {}", spaces);
+    
+    // Tuple types
+    let tuple: (i32, f64, bool) = (500, 23.34, true);
+    println!("Tuple: {:?}", tuple);
+
+    // Array types
+    let array: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    println!("Array: {:?}", array);
 }
