3f7c3f5a28845b46e3e607c0650e043dadcd8ce4
RitchielRez <ritchielrez@gmail.com>
2021-09-16 22:09:59 +0600

    Don't track .vscode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let mut x = 185;
 
@@ -13,8 +15,10 @@ fn main() {
     // Tuple types
     let tuple: (i32, f64, bool) = (500, 23.34, true);
     println!("Tuple: {:?}", tuple);
+    println!("Tuple: {:?}", tuple.2);
 
     // Array types
     let array: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     println!("Array: {:?}", array);
+    println!("Array 1: {}", array[0]);
 }
